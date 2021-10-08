2f4dd9fbb2dbbd5bb2bc02189a491c2a9dcc55ac
herbanarhitech <jeff.badette@protonmail.com>
2021-10-07 22:46:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Practicing getters and setters
---
+++ src/gettersandsetters/CreateComputer.java
@@ -0,0 +1,15 @@
+package gettersandsetters;
+
+public class CreateComputer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String computer= "AMD RYzen";
+		
+		System.out.printf("%s", computer);
+		
+		
+	}
+
+}
